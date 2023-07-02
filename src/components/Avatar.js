103f34f33c33d28763fbcd4f70e34fe59a7b325b
saez-tomas <saezjuantomas@outlook.com>
2023-07-02 20:56:50 -0300

    edit

---
+++ src/components/Avatar.js
@@ -0,0 +1,9 @@
+const Avatar = () => {
+    return (
+        <div>
+            <img scr="http://via.placeholder.com/250x250"/>
+        </div>        
+    )
+};
+
+export default Avatar;
---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+const Card = () => {
+    return(
+        <div className="card">
+            Aqui va lo que sea edit
+        </div>
+    )
+}
+
+export default Card;
